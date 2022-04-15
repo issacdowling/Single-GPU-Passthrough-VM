85db73b3a20984e1955d86ba79c285a0d097a060
Issac Dowling <61799451+IssacDowling@users.noreply.github.com>
2022-04-15 10:36:44 +0100

    Update post-install-fedora-35.sh

--- post-install-fedora-35.sh
+++ post-install-fedora-35.sh
@@ -1,3 +1,5 @@
+##Run sudo nano /etc/dnf/dnf.conf , and add defaultyes=True
+
 dnf remove gnome-boxes gnome-contacts gnome-tour gnome-photos gnome-videos totem simple-scan rhythmbox cheese firefox #Totem is gnome videos, simple-scan is gnome document scanner
 
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
