ad755e79039955dc54fa2b6efd2b1f7f0e36e25e
Issac Dowling <61799451+IssacDowling@users.noreply.github.com>
2022-04-15 13:13:59 +0100

    Delete post-install-fedora-35.sh

--- post-install-fedora-35.sh
+++
@@ -1,7 +0,0 @@
-##Run sudo nano /etc/dnf/dnf.conf , and add defaultyes=True
-
-dnf remove gnome-boxes gnome-contacts gnome-tour gnome-photos gnome-videos totem simple-scan rhythmbox cheese firefox #Totem is gnome videos, simple-scan is gnome document scanner
-
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-
-flatpak install com.github.iwalton3.jellyfin-media-player com.discordapp.Discord com.obsproject.Studio com.github.wwmm.easyeffects com.bitwarden.desktop org.mozilla.firefox
